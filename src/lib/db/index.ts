3e17bd82141a9c44c5b1afaeb12ccd6679409cf9
zahra1103630 <f.z.kordlo@gmail.com>
2026-08-09 17:18:42 +0400

    chore: update database configuration

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,6 +1,16 @@
+// import { drizzle } from "drizzle-orm/neon-http";
+// import { neon } from "@neondatabase/serverless";
+// import * as schema from "./schema";
+
+// const sql = neon(process.env.DATABASE_URL!);
+// export const db = drizzle(sql, { schema });
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
+
 import * as schema from "./schema";
 
+console.log("DATABASE_URL EXISTS:", !!process.env.DATABASE_URL);
+
 const sql = neon(process.env.DATABASE_URL!);
+
 export const db = drizzle(sql, { schema });
